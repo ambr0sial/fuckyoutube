5b4ffbe9f9453cbc15994d217bbead3f43196fa3
waw <81994421+ambr0sial@users.noreply.github.com>
2023-10-15 04:12:20 +0200

    1.1 (resolution update)
    added the functionality for users to choose the video resolution (not available on the pure version for now, sorry)

--- fuckyoutube.py
+++ fuckyoutube.py
@@ -1,10 +1,24 @@
 import re
 from pytube import YouTube
 
-def get_direct_link(youtube_url):
+def get_direct_link(youtube_url, selected_quality=None):
     try:
         yt = YouTube(youtube_url)
-        stream = yt.streams.get_highest_resolution()
+        streams = yt.streams.filter(progressive=True, file_extension="mp4")
+        
+        if not streams:
+            return "could not find anything"
+        
+        if selected_quality:
+            # Filter streams by quality
+            selected_streams = [s for s in streams if selected_quality in s.resolution]
+            if selected_streams:
+                stream = selected_streams[0]
+            else:
+                return f"selected quality '{selected_quality}' not available. available resolutions: {', '.join([s.resolution for s in streams])}"
+        else:
+            stream = yt.streams.get_highest_resolution()
+            
         direct_link = stream.url
         return direct_link
     except Exception as e:
@@ -13,10 +27,19 @@ def get_direct_link(youtube_url):
 if __name__ == "__main__":
     youtube_url = input("url: ")
     
-    direct_link = get_direct_link(youtube_url)
+    # List available qualities
+    yt = YouTube(youtube_url)
+    available_qualities = [s.resolution for s in yt.streams.filter(progressive=True, file_extension="mp4")]
+    print("available resolutions:")
+    for quality in available_qualities:
+        print(quality)
+    
+    selected_quality = input("enter the desired resolution (or press ENTER for the highest resolution): ")
+    
+    direct_link = get_direct_link(youtube_url, selected_quality)
     
     if direct_link:
-        print("direct link:")
+        print("direct link:\n")
         print(direct_link)
     else:
-        print("could not retrieve the link")
+        print("could not retrieve")
